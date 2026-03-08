50ff10bc3ded8e4755b9f628bfa0dbd207e8020f
Ayato.M <ayato1651@icloud.com>
2026-03-08 23:28:37 +0900

    Initial commit

---
+++ nvim/lua/options.lua
@@ -0,0 +1,11 @@
+o = vim.o
+g = vim.g
+
+o.number = true
+o.relativenumber = true
+
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+g.mapleader = " "
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+config.window_background_opacity = 0.85
+
+return config
